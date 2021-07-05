06b7b1b6da875733f2c4415e18a96c98f127754b
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-07-05 22:01:29 +0900

    무한스크롤 구현

--- image-community/src/pages/PostList.js
+++ image-community/src/pages/PostList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import InfinityScroll from "../shared/InfinityScroll";
 
 import Post from "../components/Post";
 import { actionCreators as postActions } from "../redux/modules/post";
@@ -8,8 +9,8 @@ const PostList = (props) => {
     const dispatch = useDispatch();
     const post_list = useSelector((state) => state.post.list);
     const user_info = useSelector((state) => state.user.user);
-
-    let is_me
+    const is_loading = useSelector((state) => state.post.is_loading);
+    const paging = useSelector((state) =>  state.post.paging);
 
     console.log(post_list);
 
@@ -24,14 +25,23 @@ const PostList = (props) => {
     return(
         <React.Fragment>
             {/* <Post/> */}
+            <InfinityScroll
+                callNext={() =>{
+                    dispatch(postActions.getPostFB(paging.next));
+                }}
+                is_next = {paging.next? true: false}
+                loading= {is_loading}
+            >
             {post_list.map((p, idx) => {
                 if(p.user_info.user_id === user_info?.uid){
                     return <Post key={p.id} {...p} is_me/>;
                 }else{
-                    return <Post key={p.id} {...p}/>
+                    return <Post key={p.id} {...p}/
+                    >
                 }
                 
             })}
+            </InfinityScroll>
         </React.Fragment>
     )
 
